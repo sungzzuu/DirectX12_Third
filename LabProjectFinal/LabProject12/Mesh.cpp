dc49a95d5fca51d7e2e62692ff8fa0de3b7c3c50
윤sungzzuu <sungzzuu@gmail.com>
2021-06-21 16:25:40 +0900

    OBJ 부분 삭제

--- LabProjectFinal/LabProject12/Mesh.cpp
+++ LabProjectFinal/LabProject12/Mesh.cpp
@@ -640,64 +640,6 @@ void CFlyerShipMeshDiffused::LoadMeshFromFile(ID3D12Device* pd3dDevice, ID3D12Gr
 			}
 		}
 	}
-	else if (bTextFile) // OBJ ÆÄÀÏ
-	{
-
-		/*FILE* file = fopen(pstrFileName, "r");
-
-		while (1) {
-			char lineHeader[128];
-
-			int res = fscanf(file, "%s", lineHeader);
-			if (res == EOF)
-				break;
-			if (strcmp(lineHeader, "v") == 0) {
-				XMFLOAT3 vertex;
-				fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
-				pVertices.push_back(vertex);
-			}
-			else if (strcmp(lineHeader, "vt") == 0) {
-				glm::vec2 uv;
-				fscanf(file, "%f %f\n", &uv.x, &uv.y);
-				temp_uvs.push_back(uv);
-			}
-			else if (strcmp(lineHeader, "vn") == 0) {
-				glm::vec3 normal;
-				fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
-				temp_normals.push_back(normal);
-			}
-			else if (strcmp(lineHeader, "f") == 0) {
-				string vertex1, vertex2, vertex3;
-				unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-				int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
-				if (matches != 9) {
-					printf("File can't be read by our simple parser : ( Try exporting with other options\n");
-					return false;
-				}
-				vertexIndices.push_back(vertexIndex[0]);
-				vertexIndices.push_back(vertexIndex[1]);
-				vertexIndices.push_back(vertexIndex[2]);
-				uvIndices.push_back(uvIndex[0]);
-				uvIndices.push_back(uvIndex[1]);
-				uvIndices.push_back(uvIndex[2]);
-				normalIndices.push_back(normalIndex[0]);
-				normalIndices.push_back(normalIndex[1]);
-				normalIndices.push_back(normalIndex[2]);
-			}
-		}
-		for (unsigned int i = 0; i < vertexIndices.size(); ++i) {
-			unsigned int vertexIndex = vertexIndices[i];
-			glm::vec3 vertex = temp_vertices[vertexIndex - 1];
-
-			unsigned int normalIndex = normalIndices[i];
-			glm::vec3 normal = temp_normals[normalIndex - 1];
-
-			object.vertices.push_back(vertex);
-			object.normals.push_back(normal);
-			object.color.push_back(_color);
-
-		}*/
-	}
 	else
 	{
 		FILE* pFile = NULL;
